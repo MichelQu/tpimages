ca2959072631e5090d1b934db7f9074fe22883d2
Maxence53 <59647293+Maxence53@users.noreply.github.com>
2020-01-09 11:06:53 +0100

    ceci est l'initialisation + l'image
    la conversion flottant a fait nimp

---
+++ Init.cpp
@@ -0,0 +1,65 @@
+
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#define ESC_KEY 27 // should be 27
+#define Q_KEY 	113 // should be 113
+
+
+using namespace std;
+using namespace cv;
+
+int main(){
+	// the OpenCV data structure storing the image
+	Mat im;
+	Mat im2;
+	int key = 0;   
+	vector<vector<cv::Mat> > matriceCoupee; 
+
+	// the (default) name of the image file
+	String imName= "image.bmp";
+
+	im = imread(imName);
+
+	if(!im.data || im.empty() || (im.rows == 0)|| (im.cols == 0)) {
+		cout << "Could not load image !" <<endl;
+		cout << "Exiting now..." << endl;
+		return 1;
+	}
+
+	int moduloRow = im.rows%8;
+	int moduloCol= im.cols%8;
+	Mat imageBonFormat(im.rows+8-moduloRow, im.cols+8-moduloCol, CV_64F , 0);
+	im.convertTo(im2,CV_64F);
+
+	//imageBonFormat.colRange(0,im.rows-1).rowRange(0,im.cols-1)=im;
+
+	for (int i=0; i<im.rows/8-1;i++){
+		Range debut(8*i,8*(i+1));
+
+		Mat ligneImage(im2, debut, Range::all());
+
+		std::vector<cv::Mat> matricesAjoutees;
+
+		for (int j=0; j<im.cols/8-1;j++){
+			Range debut2(8*j,8*(j+1));
+			Mat uneLigneImage(ligneImage,Range::all(),debut2);
+			matricesAjoutees.push_back(uneLigneImage);
+			
+		}
+		matriceCoupee.push_back(matricesAjoutees);
+
+	}
+	while ( (key != ESC_KEY) && (key!= Q_KEY) ) {
+		// Creating a window to display some images
+		namedWindow("Original image");
+		// Displaying the loaded image in the named window
+		imshow("Original image", im2);
+		key = waitKey();
+	}
+
+
+	return 0;
+}
