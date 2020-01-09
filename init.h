83c8e6518fa1982903d31b418dd595d32eb2ad80
Maxence53 <59647293+Maxence53@users.noreply.github.com>
2020-01-09 11:32:35 +0100

    Add files via upload

---
+++ init.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#define ESC_KEY 27 // should be 27
+#define Q_KEY 	113 // should be 113
+
+
+using namespace std;
+using namespace cv;
+
+std::vector<std::vector<cv::Mat> > init();
