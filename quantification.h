e42722bf8249643b07c1902e26e56fad974e3340
saberkane2018 <57622622+saberkane2018@users.noreply.github.com>
2020-01-09 10:34:20 +0100

    Add files via upload

---
+++ quantification.h
@@ -0,0 +1,7 @@
+#include <opencv/cv.hpp>
+
+
+using namespace cv;
+
+Mat quantification(Mat m, int qualite); // m : matrice d'entrée (résultat de la transf discrete , qualite : facteur de qualité (entre 0 et 25) 
+Mat dequantification(Mat m, int qualite); // m : matrice quantifiée , qualite : le même facteur que pour la quantification
