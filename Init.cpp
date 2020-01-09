09f4a3a13b52c80064f73353d5b21a178109eea7
Maxence53 <59647293+Maxence53@users.noreply.github.com>
2020-01-09 11:34:12 +0100

    Update Init.cpp

--- Init.cpp
+++ Init.cpp
@@ -1,12 +1,4 @@
-
-#include <iostream>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-
-#define ESC_KEY 27 // should be 27
-#define Q_KEY 	113 // should be 113
-
+#include "init.h"
 
 using namespace std;
 using namespace cv;
